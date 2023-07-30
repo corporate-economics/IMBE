33af897c472c864537605115e1414ea4577312ea
corporate-economics <corporate.economics@gmail.com>
2023-07-30 19:30:07 +0530

    2nd commit for the package

---
+++ R/hello.R
@@ -0,0 +1,30 @@
+
+#' Hello my world function
+#' @param x is the name of the person to say hi
+#'
+#' @return The output from \code{\link{print}}
+#' @export
+#'
+#' @examples
+#' hello("Jhon")
+#' \dontrun{
+#' hello("Hasu")
+#' }
+hello <- function(x) {
+  print(paste("Hello", x,", This is the world", sep= " "))
+}
+
+
+#' Link to the data in the MCBE package
+#'
+#' @param x name of the file
+#'
+#' @return The output is the path to the file in the local system
+#' @export
+#'
+#' @examples
+#' data.link ("callMoney.xlsx")
+data.link <- function(x){
+ system.file("extdata", x, package = "MCBE")
+}
+
